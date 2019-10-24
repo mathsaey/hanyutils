1b2afd4250ca2a02b40ea3fd034d07e411bb6c64
Mathijs Saey <mathsaey@vub.ac.be>
2019-10-24 16:08:11 +0200

    Prepare for hex publish

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule Hanyutils.MixProject do
       app: :hanyutils,
       version: "0.1.0",
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -14,6 +15,18 @@ defmodule Hanyutils.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Utilities for dealing with Chinese characters and pinyin."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/mathsaey/hanyutils"},
+      source_url: "https://github.com/mathsaey/hanyutils"
     ]
   end
 end
