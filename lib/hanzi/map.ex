0336ee830eaaaefdbf7f984e6bbe39e83d00cb55
Mathijs Saey <mathijs@mathsaey.be>
2020-09-08 17:22:59 +0200

    Fix incorrect most common pronunciations

--- lib/hanzi/map.ex
+++ lib/hanzi/map.ex
@@ -27,16 +27,21 @@ defmodule Hanzi.Map do
   |> Stream.map(fn
     # If alt only repeats the most common pronunciation we drop it
     m = %{pron: p, alt: [p]} -> Map.delete(m, :alt)
+    # Don't modify entries with both pronunciation and alt after this point
+    t = %{char: _, alt: _, pron: _} -> t
     # Words that occur only in kHanyuPinyin
     %{char: c, alt: [p]} -> %{char: c, pron: p}
     # Words that occur only in kHanyuPinyin with multiple pronunciations
     m = %{alt: [p | _]} -> Map.put(m, :pron, p)
-    any -> any
+    # Entries with only pronunciation remain unmodified, use this instead of a catch-all clause
+    # to prevent bugs
+    t = %{char: _, pron: _} -> t
   end)
   |> Enum.map(&struct(Hanzi, &1))
   |> Enum.map(fn s = %Hanzi{char: c} ->
     def lookup(unquote(c)), do: unquote(Macro.escape(s))
   end)
+
   # For using during development
   # |> Enum.map(fn s = %{char: c} ->
   #   def lookup(unquote(c)), do: struct(Hanzi, unquote(Macro.escape(s)))
