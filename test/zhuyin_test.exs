7576069c24ae2659e5fe148cc971c021973b33e4
Mathijs Saey <mathijs@mathsaey.be>
2022-05-03 22:06:20 +0200

    Reorganize tests

--- test/zhuyin_test.exs
+++ test/zhuyin_test.exs
@@ -4,8 +4,8 @@ defmodule ZhuyinTest do
   import Pinyin
   doctest Zhuyin
 
-  describe "Decode Zhuyin" do
-    test "Full syllables" do
+  describe "read" do
+    test "full syllables" do
       tests = [
         {"ㄐㄩ˙", ~p/jv/},
         {"ㄌㄩˇ", ~p/lv3/},
@@ -33,80 +33,70 @@ defmodule ZhuyinTest do
       Enum.each(tests, fn {z, p} -> assert Zhuyin.read!(z) |> Zhuyin.to_pinyin() == p end)
     end
 
-    test "Incomplete syllables" do
-      tests = [
-        "ㄝ",
-        "ㄐˇ"
-      ]
-
+    test "incomplete syllables" do
+      tests = ~w(ㄝ ㄐˇ)
       Enum.each(tests, fn z -> assert Zhuyin.read(z) == {:error, z} end)
     end
 
-    test "Standalone initials" do
-      # First a regular syllable
-      {:ok, [%Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 3}], "", %{}, {1, 0}, 8} =
-        Zhuyin.Parsers.syllable("ㄋㄧˇ")
+    test "regular standalone initials" do
+      assert Zhuyin.read!("ㄋㄧˇ") == [%Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 3}]
+    end
 
-      # Must not allow standalone initials be combined with a final
-      assert {:error, "expected end of string", "ㄧˇ", %{}, {1, 0}, 3} ==
-               Zhuyin.Parsers.syllable("ㄕㄧˇ")
+    test "exclusive mode" do
+      assert Zhuyin.read("ㄕㄧˇ", :exclusive) == {:ok, [~z/ㄕ/s, ~z/ㄧˇ/s]}
+      assert Zhuyin.read("ㄋㄧˇㄏㄠˇ", :exclusive) == {:ok, [~z/ㄋㄧˇ/s, ~z/ㄏㄠˇ/s]}
+    end
 
-      # That can only be parsed if we allow multiple words
-      assert {:ok, [~z/ㄕ/, ~z/ㄧˇ/] |> List.flatten(), "", %{}, {1, 0}, 8} ==
-               Zhuyin.Parsers.zhuyin_only("ㄕㄧˇ")
+    test "exclusive mode does not accept mixed strings" do
+      assert Zhuyin.read("ㄋㄧˇhao", :exclusive) == {:error, "ㄋㄧˇhao"}
+    end
+
+    test "can parse multiple words with one or more syllables" do
+      assert Zhuyin.read("ㄋㄧˇㄏㄠˇㄨˇ ㄕˋ ㄋㄧˇ ㄍㄜㄍㄜ˙", :exclusive) ==
+               {:ok,
+                [
+                  ~z/ㄋㄧˇ/s,
+                  ~z/ㄏㄠˇ/s,
+                  ~z/ㄨˇ/s,
+                  " ",
+                  ~z/ㄕˋ/s,
+                  " ",
+                  ~z/ㄋㄧˇ/s,
+                  " ",
+                  ~z/ㄍㄜ/s,
+                  ~z/ㄍㄜ˙/s
+                ]}
+    end
+
+    test "mixed strings" do
+      assert Zhuyin.read!("test ㄓㄨyu", :mixed) |> Zhuyin.to_pinyin() |> Pinyin.marked() ==
+               "test zhūyu"
     end
   end
 
-  describe "Syllable parser" do
-    test "Syllable parser parses only a single syllable" do
-      {:error, "expected end of string", "ㄏㄠˇ", %{}, {1, 0}, 8} =
-        Zhuyin.Parsers.syllable("ㄋㄧˇㄏㄠˇ")
+  describe "from_string" do
+    test "only parses a single syllable" do
+      assert Zhuyin.from_string("ㄋㄧˇㄏㄠˇ") == {:error, "ㄏㄠˇ"}
     end
 
-    test "Works for syllable with initial, final and tone" do
-      {:ok, [%Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 3}], "", %{}, {1, 0}, 8} =
-        Zhuyin.Parsers.syllable("ㄋㄧˇ")
+    test "works for syllables with initial, final and tone" do
+      assert Zhuyin.from_string("ㄋㄧˇ") == {:ok, %Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 3}}
     end
 
-    test "Works for syllable with initial and final, no tone" do
-      {:ok, [%Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 1}], "", %{}, {1, 0}, 6} =
-        Zhuyin.Parsers.syllable("ㄋㄧ")
+    test "works for syllables with initial, final and no tone" do
+      assert Zhuyin.from_string("ㄋㄧ") == {:ok, %Zhuyin{initial: "ㄋ", final: "ㄧ", tone: 1}}
     end
 
-    test "Works for standalone final" do
-      {:ok, [%Zhuyin{initial: "", final: "ㄧ", tone: 1}], "", %{}, {1, 0}, 3} =
-        Zhuyin.Parsers.syllable("ㄧ")
+    test "works for standalone finals" do
+      assert Zhuyin.from_string("ㄧ") == {:ok, %Zhuyin{initial: "", final: "ㄧ", tone: 1}}
     end
 
-    test "Doesn't work for character that's not a standalone final" do
-      {:error, _, "ㄋ", %{}, {1, 0}, 0} = Zhuyin.Parsers.syllable("ㄋ")
-    end
-  end
-
-  describe "Zhuyin only parser" do
-    test "Can parse a full word of two syllables" do
-      assert {:ok, [~z/ㄋㄧˇ/, ~z/ㄏㄠˇ/] |> List.flatten(), "", %{}, {1, 0}, 16} ==
-               Zhuyin.Parsers.zhuyin_only("ㄋㄧˇㄏㄠˇ")
+    test "doesn't work for characters that are not standalone finals" do
+      assert Zhuyin.from_string("ㄋ") == {:error, "ㄋ"}
     end
 
-    test "Doesn't accept mixed strings" do
-      {:error, "expected end of string", "ㄋㄧˇhao", %{}, {1, 0}, 0} =
-        Zhuyin.Parsers.zhuyin_only("ㄋㄧˇhao")
-    end
-
-    test "Can parse a multiple words with one or more syllables." do
-      assert {:ok,
-              [~z/ㄋㄧˇ/, ~z/ㄏㄠˇ/, ~z/ㄨˇ/, " ", ~z/ㄕˋ/, " ", ~z/ㄋㄧˇ/, " ", ~z/ㄍㄜ/, ~z/ㄍㄜ˙/]
-              |> List.flatten(), "", %{}, {1, 0},
-              51} ==
-               Zhuyin.Parsers.zhuyin_only("ㄋㄧˇㄏㄠˇㄨˇ ㄕˋ ㄋㄧˇ ㄍㄜㄍㄜ˙")
-    end
-  end
-
-  describe "Mixed parser" do
-    test "" do
-      pinyin = Zhuyin.read!("test ㄓㄨyu", :mixed) |> Zhuyin.to_pinyin() |> Enum.join()
-      assert pinyin == "test zhūyu"
+    test "does not allow standalone initials to be combined with a final" do
+      assert Zhuyin.from_string("ㄕㄧˇ") == {:error, "ㄧˇ"}
     end
   end
 end
